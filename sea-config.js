223f429b5ddf3ab4dfd9c68e3f442a0160b6feb8
zhanglei923 <zhang.lei.923@gmail.com>
2018-06-02 23:06:45 +0800

    update

---
+++ sea-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    sourceRoot: 'E:/workspaces/node-seajs/cmdCode/',
+    alias: {
+        'rk': 'core/rkloader',
+        'fancymap': 'lib/fancymap.js',
+        'commonjs': 'core/widgets/common.js',
+        'globaljs': 'core/widgets/global.js',
+        'userService': 'core/services/2.0/rk_userService',
+        'workreportService': 'core/services/2.0/rk_workreportService',
+        'noticeService': 'core/services/biz/noticeService',
+        'xsy': 'platform/core/xsyloader',
+        'scheduleService': 'core/services/biz/scheduleService'
+    }
+}
